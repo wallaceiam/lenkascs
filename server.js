58210f1852c5de30c5678fae2d59376bb508f872
wallaceiam <chris@mymojo.ca>
2018-08-20 23:43:31 +0100

    hot reload

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-/**
- * Static HTTP Server
- *
- * Create a static file server instance to serve files
- * and folder in the './public' folder
- */
+var express = require('express')
+var http = require('http')
+var path = require('path')
+var reload = require('reload')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+var watch = require('watch')
 
-// modules
-var static = require( 'node-static' ),
-    port = 8080,
-    http = require( 'http' );
+var app = express()
 
-// config
-var file = new static.Server( './public', {
-    cache: 3600,
-    gzip: true
-} );
+var publicDir = path.join(__dirname, 'public')
 
-// serve
-http.createServer( function ( request, response ) {
-    request.addListener( 'end', function () {
-        file.serve( request, response );
-    } ).resume();
-} ).listen( port );
+console.log(publicDir);
+
+app.set('port', process.env.PORT || 3000)
+app.use(logger('dev'))
+app.use(bodyParser.json()) // Parses json, multi-part (file), url-encoded
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(publicDir, 'index.html'))
+})
+
+var server = http.createServer(app)
+
+// Reload code here
+var reloadServer = reload(app, {verbose: true});
+watch.watchTree(__dirname + "/public", function (f, curr, prev) {
+    // Fire server-side reload event
+    reloadServer.reload();
+});
+
+server.listen(app.get('port'), function () {
+    console.log('Web server listening on port ' + app.get('port'))
+})
